d12a805d58ffee846d49625417318f8afca86453
Jacky Sum <jsum@pivotal.io>
2018-03-02 14:00:02 -0700

    update yaml_as to yaml_tag
    Signed-off-by: Jeff Turley <jturley@pivotal.io>

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -1,6 +1,6 @@
 if defined?(ActiveRecord)
   class ActiveRecord::Base
-    yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
+    yaml_tag "tag:ruby.yaml.org,2002:ActiveRecord"
 
     def self.yaml_new(klass, tag, val)
       klass.unscoped.find(val['attributes'][klass.primary_key])
